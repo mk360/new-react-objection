c2a595c323ae27178d58d28a9d65943b07014994
mk360 <hamoudi191@hotmail.fr>
2021-12-19 21:55:50 +0100

    character selection

--- src/components/character-button/index.tsx
+++ src/components/character-button/index.tsx
@@ -4,16 +4,22 @@ import './style.scss';
 const componentName = 'character-button';
 
 const CharacterButton = (props: CharacterButtonProps) => {
-  const { cornerClass, character, onSelection } = props;
+  const { cornerClass, character, onSelection, selected } = props;
   const classes = [componentName];
+  const selectionClass = selected ? 'selected' : '';
 
   if (cornerClass) {
     classes.push(cornerClass);
   }
 
+  const clickHandler = () => {
+    onSelection(character);
+  };
+
   return (
     <button
-      className={`${character.name} ${classes.join('-')}`}
+      className={`${character.name} ${classes.join('-')} ${selectionClass}`}
+      onClick={clickHandler}
     >
       {character.name} {/* until i somehow style it better */}
     </button>
--- src/components/character-button/props.ts
+++ src/components/character-button/props.ts
@@ -3,6 +3,7 @@ import Character from "../../common/character";
 
 interface CharacterButtonProps {
   character: Character;
+  selected: boolean;
   onSelection: (character: Character) => void;
   cornerClass?: ButtonCorner;
 };
